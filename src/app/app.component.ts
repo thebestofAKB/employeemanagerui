9f21937fbbb4016f9e9c268cf36500ac5ac2929e
thebestofAKB <abdoulkarimbalde25@gmail.com>
2022-07-25 00:48:37 +0000

    importing ui files in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './Employee';
 import { EmployeeService } from './employee.service';
 
@@ -30,23 +31,36 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
   public onOpenModal(employee: Employee|null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement("button");
 
     button.type = "button";
     button.style.display = "none";
-    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-bs-toggle', 'modal');
     if (mode === 'add'){
-      button.setAttribute('data-target', '#addEmployeeModal');
+      button.setAttribute('data-bs-target', '#addEmployeeModal');
     }
 
     if (mode === 'edit'){
-      button.setAttribute('data-target', '#updateEmployeeModal');
+      button.setAttribute('data-bs-target', '#updateEmployeeModal');
     }
 
     if (mode === 'delete'){
-      button.setAttribute('data-target', '#deleteEmployeeModal');
+      button.setAttribute('data-bs-target', '#deleteEmployeeModal');
     }
 
     container?.appendChild(button);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
